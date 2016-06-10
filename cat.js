4661464ace1040454685a71aa0543e2d92eea406
Joseph Daniel Chapdelaine <orvist@gmail.com>
2016-06-10 13:30:30 -0500

    added catjson/js and printed new json to page

--- cat.js
+++ cat.js
@@ -1,31 +1,37 @@
-var dogContainer = document.getElementById("forDogs");
+var catContainer = document.getElementById("forCats");
 // Your first task is to build a product page that displays all brands of dog food, the different types, with the price and size for each container volume
-function	getData () {
+function	getDataCats () {
 	var data = JSON.parse(event.target.responseText);
-	var currentBrand = " ";
+	var currentBrand = "";
 	var brand;
 	var type;
 	var volume;
 	var price;
-// Loop through dog_brands
-	for (i = 0; i < data.dog_brands.length; i++) {
+	var breed;
+
+// Loop through cat_brands
+	for (i = 0; i < data.cat_brands.length; i++) {
 		// variable to hold obj prop
-		brand = data.dog_brands[i].name;
+		brand = data.cat_brands[i].name;
+		breed = data.cat_brands[i].breeds.join(", ");
 		// create div wih unique id
 		currentBrand += `<div>`;
 		currentBrand += `<h1 id="b${i + 1}" class="brand">`;
 		currentBrand += `${brand}`;
+		currentBrand += `<h2 class="breed">`;
+		currentBrand += `Breeds: ${breed}`;
+		currentBrand += `</h2>`;
 		currentBrand += `</h1>`;
 		// repeat till we get through object
-		for (j = 0; j < data.dog_brands.length; j++) {
-			type = data.dog_brands[i].types[j].type;
+		for (j = 0; j < data.cat_brands.length; j++) {
+			type = data.cat_brands[i].types[j].type;
 			currentBrand += `<div class="infoDiv">`;
 			currentBrand += `<h2>`;
 		  currentBrand += `${type} :`;
 		  currentBrand += `</h2>`;
-	 	  for (x = 0; x < data.dog_brands.length; x++) {
-				volume = data.dog_brands[i].types[j].volumes[x].name;
-				price = data.dog_brands[i].types[j].volumes[x].price;
+	 	  for (x = 0; x < data.cat_brands[i].types[j].volumes.length; x++) {
+				volume = data.cat_brands[i].types[j].volumes[x].name;
+				price = data.cat_brands[i].types[j].volumes[x].price;
 				currentBrand += `<div>`;
 				currentBrand += `<h2>`;
 		  	currentBrand += `${volume} `;
@@ -38,7 +44,7 @@ function	getData () {
 		}	
 				currentBrand += `</div>`;
 	}
-	dogContainer.innerHTML = currentBrand;
+	catContainer.innerHTML = currentBrand;
 }
 
 
@@ -46,11 +52,10 @@ function displayError() {
 	alert("ERROR");
 }
 
-
 var requestProduct = new XMLHttpRequest();
 
-requestProduct.addEventListener("load", getData);
+requestProduct.addEventListener("load", getDataCats);
 requestProduct.addEventListener("error", displayError);
 
-requestProduct.open("GET", "dog_brands.json");
+requestProduct.open("GET", "cat.json");
 requestProduct.send();
